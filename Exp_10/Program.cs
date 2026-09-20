091559b09a8fe54594e408bd7f2f83001bd6e0eb
krishnakrishna001 <krishnablue12@gmail.com>
2026-09-20 14:05:12 +0530

    Add files via upload

---
+++ Exp_10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string con = @"Data Source=.\SQLEXPRESS;Initial Catalog=College;Integrated Security=True;TrustServerCertificate=True";
+
+        SqlDataAdapter da =
+            new SqlDataAdapter("SELECT * FROM Student", con);
+
+        DataSet ds = new DataSet();
+
+        da.Fill(ds, "Student");
+
+        foreach (DataRow row in ds.Tables["Student"].Rows)
+        {
+            Console.WriteLine(row["Id"] + " " + row["Name"]);
+        }
+    }
+}
